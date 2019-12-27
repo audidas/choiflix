5c640d6036c7acdf101a6253cf92695c79929b1c
unknown <audidas@gmail.com>
2019-12-27 11:35:43 +0900

    Updates

---
+++ precache-manifest.fcbec73f8d93cc9824dad58b820be8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3db5b99cb853efcd72367ae61d0182",
+    "url": "/choiflix/index.html"
+  },
+  {
+    "revision": "2f43bc165642a28b01cb",
+    "url": "/choiflix/static/js/2.515c6dde.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/choiflix/static/js/2.515c6dde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a0d8849ba814e47d903",
+    "url": "/choiflix/static/js/main.6b633317.chunk.js"
+  },
+  {
+    "revision": "762be1c85a60865192bf",
+    "url": "/choiflix/static/js/runtime-main.27932377.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/choiflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
